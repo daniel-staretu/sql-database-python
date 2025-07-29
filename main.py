febf959a62b8474e0b2cecbc81b709ac1678c90d
Staretu Sebastian-Daniel <daniel.sebastianstaretu@gmail.com>
2025-07-29 18:50:21 +0300

    updated main

--- main.py
+++ main.py
@@ -2,8 +2,4 @@ import database as db
 import queries as q
 
 if __name__ == '__main__':
-    connection = db.create_server_connection()
-    query = "CREATE DATABASE test"
-
-    db.create_database(connection, query)
-    db.create_db_connection("test")
+    pass
